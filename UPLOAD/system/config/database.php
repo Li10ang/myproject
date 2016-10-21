270d8c9199917b8a05a118b4f9a67e59da0b41dd
Li10ang <935162938@qq.com>
2016-10-21 18:42:07 +0800

    blog test#
    ^X

---
+++ UPLOAD/system/config/database.php
@@ -0,0 +1,13 @@
+<?php
+
+$config['charset'] = 'utf8';
+$config['prefix'] = 'aws_';
+$config['driver'] = 'MySQLi';
+$config['master'] = array (
+  'charset' => 'utf8',
+  'host' => '120.27.7.144',
+  'username' => 'root',
+  'password' => '428686',
+  'dbname' => 'wecenter',
+);
+$config['slave'] = false;
---
+++ hexo/source/_posts/The-new-article.md
@@ -0,0 +1,5 @@
+---
+title: The new article
+date: 2016-10-21 18:15:51
+tags:
+---
