64a1fcc74bdf57fde998bcca3fb5877d846b8042
姚亮 <935162938@qq.com>
2017-07-16 03:51:24 +0800

    数据

---
+++ dn-project/project1/list.js
@@ -0,0 +1,26 @@
+getData([
+  {
+  	"id":1,
+    "img": "assets/images/p1.jpg",
+    "name": "创意烤漆加厚云朵隔板搁板墙壁置物架客厅影视墙装饰架装修层板",
+    "amount": 30,
+    "count":1,
+    "total": 30
+  },
+  {
+  	"id":2,
+    "img": "assets/images/p2.jpg",
+    "name": "隔板搁板墙壁置物架客创意烤漆加厚云朵厅影视墙装饰架装修层板",
+    "amount": 40,
+    "count":2,
+    "total": 80
+  },
+  {
+  	"id":3,
+    "img": "assets/images/p3.jpg",
+    "name": "壁置物架客厅影视墙装饰架装修层板创意烤漆加厚云朵隔板搁板墙",
+    "amount": 50,
+    "count":3,
+    "total": 150
+  }
+]);
