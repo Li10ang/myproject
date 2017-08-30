f48a5c4ccf84df179627a194d43ce0271f2dcde3
Yl428686 <935162938@qq.com>
2017-08-30 15:17:17 +0800

    weather

--- weather/weather.php
+++ weather/weather.php
@@ -22,5 +22,6 @@
         curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
     }
-    var_dump(curl_exec($curl));
+    // var_dump(curl_exec($curl));
+    echo json_decode(curl_exec($curl))
 ?>
